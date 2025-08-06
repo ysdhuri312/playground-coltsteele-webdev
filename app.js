f35b6bf1918021c2efbf4896d4414ca95e216a12
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-08-06 23:10:40 +0530

    Setup map using  maptiler on home route

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 /** @format */
 
 import express from 'express';
+import path from 'node:path';
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(path.resolve(), 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.json({ message: 'hello' });
+  res.render('map');
+});
+
+app.post('/', (req, res) => {
+  const location = req.body.location;
+  res.json({ message: location });
 });
 
 const PORT = 3000;
